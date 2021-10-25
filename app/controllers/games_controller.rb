86b636b56f006de723d7aac8862f0f95fe80448c
houzairkoussa <houzairmk@icloud.com>
2021-10-25 15:55:06 +0400

    exercise 3 rails day 1. TODO: session and score

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,24 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
+
+  def score
+    @score = 0
+    dictionary_api_url = "https://wagon-dictionary.herokuapp.com/#{params[:answer]}"
+    word = JSON.parse(URI.open(dictionary_api_url).read)
+    if word['found'] == true && !params[:answer].nil?
+      if params[:answer].chars.all? {|letter| params[:answer].count(letter) <= params[:grid].count(letter)}
+        @output = "Congratulations! #{params[:answer].upcase} is a valid English Word!"
+        @score += params[:answer].length * 2
+      else
+        @output = "Sorry but #{params[:answer].upcase} can't be built from #{params[:grid].upcase}"
+      end
+    else
+      @output = "Sorry but #{params[:answer].upcase} is not an English Word"
+    end
+  end
+end
